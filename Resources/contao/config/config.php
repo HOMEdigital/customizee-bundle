d767ab51ab62bf8df8ffc9dda32d03b896876463
Dirk Holstein <dh@holsteinmedia.com>
2018-12-03 10:46:06 +0100

    Custumizee aufgeräumt. Inhalte sind jetzt Bestandteil von kitee-bundle

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -6,15 +6,3 @@
  * Time: 14:50
  */
 
-#-- hooks --------------------------------------------------------------------------------------------------------------
-$GLOBALS['TL_HOOKS']['getArticle'][] = array('Home\CustomizeeBundle\Resources\contao\hooks\GetArticle', 'setCustomClasses');
-
-#-- redirects ----------------------------------------------------------------------------------------------------------
-$GLOBALS['BE_MOD']['design']['Module'] = array(
-    'callback'   => 'Home\PearlsBundle\Resources\contao\Helper\BeModRedirect',
-    'action'     => array('link'=>'/contao?do=themes&table=tl_module&id=1'),
-);
-#$GLOBALS['BE_MOD']['design']['Main-Layout'] = array(
-#    'callback'   => 'Home\PearlsBundle\Resources\contao\Helper\BeModRedirect',
-#    'action'     => array('link'=>'/contao?do=themes&table=tl_layout&id=1&act=edit'),
-#);
